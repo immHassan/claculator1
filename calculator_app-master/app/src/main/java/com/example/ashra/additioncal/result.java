7e5866bcf32f13195b145280cc2fa506119d783b
muhammadali92 <alii.engineerr@gmail.com>
2018-10-10 15:45:52 +0500

    first commit

---
+++ calculator_app-master/app/src/main/java/com/example/ashra/additioncal/result.java
@@ -0,0 +1,28 @@
+package com.example.ashra.additioncal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+public class result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        try
+        {
+            TextView answer;
+            answer = findViewById(R.id.ans);
+            answer.setText(Objects.requireNonNull(getIntent().getExtras()).getString("key"));
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(this,"Exception Raised", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
